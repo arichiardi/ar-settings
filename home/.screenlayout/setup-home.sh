5715d63cb00df5b51f5040bf15ef582463c754e6
Andrea Richiardi <a.richiardi.work@gmail.com>
2024-08-05 16:58:26 -0600

    Update randr home screen layout

--- home/.screenlayout/setup-home.sh
+++ home/.screenlayout/setup-home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-A-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-0 --mode 1920x1080 --pos 3840x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
+xrandr --output eDP --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-A-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DisplayPort-0 --mode 2560x1440 --pos 4480x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off
