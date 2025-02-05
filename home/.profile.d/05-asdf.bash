6458352579a30bc5625f75d3f340eee337bcda59
Andrea Richiardi <a.richiardi.work@gmail.com>
2025-02-05 13:01:11 -0700

    Update asdf files to new 0.15.0 setup

--- home/.profile.d/05-asdf.bash
+++ home/.profile.d/05-asdf.bash
@@ -1,8 +1,3 @@
-is_os darwin && asdf_script_dir=${HOMEBREW_PREFIX}/opt/asdf/libexec
-is_os linux && asdf_script_dir=/opt/asdf-vm
+asdf_data_dir=${ASDF_DATA_DIR:-$HOME/.asdf}
 
-# https://asdf-vm.com
-source $asdf_script_dir/asdf.sh
-
-# https://github.com/halcyon/asdf-java?tab=readme-ov-file#java_home
-source $HOME/.asdf/plugins/java/set-java-home.bash
+pathprepend "$asdf_data_dir/shims" PATH
