b8df19468661e49158828443a7dc671a4406dd85
Andrea Richiardi <a.richiardi.work@gmail.com>
2025-07-21 07:44:19 -0600

    Tweak the tty setup in .profile and .bashrc

--- home/.profile.d/04-gpg.bash
+++ home/.profile.d/04-gpg.bash
@@ -1,7 +1,9 @@
 # https://wiki.archlinux.org/index.php/GnuPG#SSH_agent
 
-export GPG_TTY=$(tty)
-gpg-connect-agent updatestartuptty /bye >/dev/null
+# This has to be done in .bashrc or pinentry won't see the right tty
+# when connecting to the server via ssh
+#export GPG_TTY=$(tty)
+#gpg-connect-agent updatestartuptty /bye >/dev/null
 
 export GNUPGCONFIG=${GNUPGHOME:-"$HOME/.gnupg/gpg-agent.conf"}
 if grep -q enable-ssh-support "$GNUPGCONFIG"; then
