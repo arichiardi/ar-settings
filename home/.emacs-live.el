4102cedf8b66e22fa3eb7e8725b8b951c5b8f271
Andrea Richiardi <a.richiardi.work@gmail.com>
2026-04-15 08:50:01 -0600

    Add home/.emacs-live.el skeleton

---
+++ home/.emacs-live.el
@@ -0,0 +1,15 @@
+;;; .emacs-live.el -- Emacs Live Custom Packs -*- lexical-binding: t; -*-
+;;
+;; Author: Andrea Richiardi
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Custom Emacs Live Packs
+
+;;; Code:
+
+;; (live-add-packs '(~/git/ar-org/emacs-pack))
+
+;;; .emacs-live.el ends here
