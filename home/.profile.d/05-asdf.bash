f39b946c77407b91192eeb30f638d96b56f691fb
Andrea Richiardi <a.richiardi.work@gmail.com>
2026-05-12 15:14:51 -0600

    Update the asdf .profile and bump .tool-versions

--- home/.profile.d/05-asdf.bash
+++ home/.profile.d/05-asdf.bash
@@ -2,7 +2,10 @@ asdf_data_dir=${ASDF_DATA_DIR:-$HOME/.asdf}
 
 PATH="$asdf_data_dir/shims":$PATH
 
+ASDF_CONFIG_FILE=$HOME/.config/asdf/asdfrc
+
+. <(asdf completion bash)
+
 # Java
 # https://github.com/halcyon/asdf-java?tab=readme-ov-file#java_home
 . ~/.asdf/plugins/java/set-java-home.bash
-
