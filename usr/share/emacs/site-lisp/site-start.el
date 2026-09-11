1d405d3dc61b380390c75c9e8406e78d559465eb
Andrea Richiardi <a.richiardi.work@gmail.com>
2026-09-11 15:05:49 -0600

    Update site-start.el with the frame decorator tweaks

--- usr/share/emacs/site-lisp/site-start.el
+++ usr/share/emacs/site-lisp/site-start.el
@@ -1,8 +1,31 @@
 (add-to-list 'default-frame-alist '(font . "JetBrainsMono Nerd Font Mono-12"))
 
-;; For some WM we want to show decorations (nil means "use decorations")
-(add-to-list 'default-frame-alist '(undecorated . nil))
-
 (set-fontset-font "fontset-default"
                   'emoji
                   (font-spec :family "Noto Color Emoji"))
+
+;; Emacs 31 loads site-start.el BEFORE early-init.el, so early-init's
+;; `(undecorated . t)' is prepended afterwards and wins.  Override it via
+;; a hook that runs after the user config but still before
+;; `frame-notice-user-settings' applies the alist to the initial frame.
+;; (when (>= emacs-major-version 31)
+;;   (add-hook 'after-init-hook
+;;             (lambda ()
+;;               (setq default-frame-alist
+;;                     (cons '(undecorated . nil)
+;;                           (assq-delete-all 'undecorated default-frame-alist))))
+;;             t))
+
+;; Show WM decorations on Linux.
+;;
+;; Emacs 29-30: site-start runs AFTER frame creation, so we must patch
+;; existing frames and update default-frame-alist for new ones.
+;; (when (<= emacs-major-version 30)
+;;   (when (eq system-type 'gnu/linux)
+;;     (setq default-frame-alist
+;;           (append (remove (lambda (x) (eq (car x) 'undecorated))
+;;                           default-frame-alist)
+;;                   '((undecorated . nil))))
+;;
+;;     (dolist (frame (frame-list))
+;;       (modify-frame-parameters frame '((undecorated . nil))))))
